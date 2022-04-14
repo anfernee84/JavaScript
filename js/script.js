c8e7c74a578ee140528e2c9d64d10fb10358fa88
Ed <eduard.rogulin@outlook.com>
2022-04-14 19:28:58 +0300

    add scripts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = "svfds";
+c = true
+console.log(typeof a);
+document.write(typeof b);
+alert(typeof c);
+console.log(typeof alert);
+console.log(typeof console);
+console.log(typeof NaN);
